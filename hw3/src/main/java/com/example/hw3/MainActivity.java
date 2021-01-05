017260a2c7a42b3490b459985fca285d989104c8
VIKTORPIZW <71723376+VIKTORPIZW@users.noreply.github.com>
2021-01-06 01:59:59 +0300

    HW3/3

--- hw3/src/main/java/com/example/hw3/MainActivity.java
+++ hw3/src/main/java/com/example/hw3/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_3_1_linear_layout);
+        setContentView(R.layout.activity_main_3_2_constraint_layout);
     }
 }
