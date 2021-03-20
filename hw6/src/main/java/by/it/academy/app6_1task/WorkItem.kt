e3e051748c14a253ff402bc4000a714f721515ad
VIKTORPIZW <71723376+VIKTORPIZW@users.noreply.github.com>
2021-03-20 13:39:53 +0300

    HW6_1

---
+++ hw6/src/main/java/by/it/academy/app6_1task/WorkItem.kt
@@ -0,0 +1,9 @@
+package by.it.academy.app6_1task
+
+class WorkItem(
+        val applicationDate: String,
+        val workType: String,
+        val cost: Float,
+        val workStatus: Int,
+)
+
---
+++ myapplication/src/main/java/com/example/homework6_1/WorkItem.kt
@@ -0,0 +1,3 @@
+package com.example.homework6_1
+
+class WorkItem(val workStatus: String, val workName: String, val workDate: String, val workCost: String)
